2496af953cf24b0538072640368a06df45a02468
taroj khan <tarujkhan@yahoo.com>
2020-03-27 00:58:10 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
-
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
+
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |course|
+puts course.text.strip
+end 
+
 #doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 #puts doc
-
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-26OIBN")
-
-
-
