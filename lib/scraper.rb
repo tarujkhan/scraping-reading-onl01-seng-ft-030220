6b698ac4cfc672d23bcf6f0be47563a7496aaeb9
taroj khan <tarujkhan@yahoo.com>
2020-04-13 16:55:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
+<<<<<<< HEAD
 
 courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
@@ -12,3 +13,20 @@ end
 
 #doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 #puts doc
+=======
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+#doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+#puts doc
+
+#doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN")
+#puts doc
+
+require 'nokogiri'
+require 'open-uri'
+
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+doc.css(".headline-260IBN")
+>>>>>>> df5222be6a48a52c797fd42ae25a2132052b812a
